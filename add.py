539749d0583459af4187597ae6ac74d76cfaf5a4
Rohit Ravi <123576106+coderRohit620@users.noreply.github.com>
2025-03-21 11:42:46 +0530

    some changes

--- add.py
+++ add.py
@@ -1,13 +1,9 @@
-class Addition:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
+class Calculator:
+    def add_numbers(a, b):
+        return a + b
 
-    def add(self):
-        return self.num1 + self.num2
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-if __name__ == "__main__":
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    addition = Addition(num1, num2)
-    print("Sum:", addition.add())
+    sum_result = add_numbers(num1, num2)
+    print("Sum:", sum_result)
