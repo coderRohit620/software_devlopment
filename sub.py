8ef4c7d2dee5d1ebf8212624114c673d0564af78
rakesh0097Bora <rakeshsinghbora224@gmail.com>
2025-03-21 11:56:56 +0530

    Delete sub.py

--- sub.py
+++
@@ -1,9 +0,0 @@
-class Calculator:
-    def mul_numbers(a, b):
-        return a * b
-
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-
-    sum_result = mul_numbers(num1, num2)
-    print("Sum:", mul_result)
