62a78fe516c87b1c15567ca6149f0dd9c124f256
Rohit Ravi <123576106+coderRohit620@users.noreply.github.com>
2025-03-23 17:40:57 +0530

    modify mul file

--- mul.py
+++ mul.py
@@ -1,9 +1,9 @@
-class Calculator:
+class Multipication:
     def mul_numbers(a, b):
         return a * b
 
     num1 = int(input("Enter first number: "))
     num2 = int(input("Enter second number: "))
 
-    sum_result = mul_numbers(num1, num2)
+    mul_result = mul_numbers(num1, num2)
     print("Sum:", mul_result)
