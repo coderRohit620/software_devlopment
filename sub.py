6cbebbaa2dea5dbe7cc913da17acc0bf698b86ba
Rohit Ravi <123576106+coderRohit620@users.noreply.github.com>
2025-03-29 01:42:39 +0530

    modify

--- sub.py
+++ sub.py
@@ -4,14 +4,5 @@ class Subtract:
         self.a = a
         self.b = b
     
-<<<<<<< HEAD
     def calculate(self):
         return self.a - self.b
-=======
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))   
-
-    sub_result = sub_number(num1,num2)
-    print("Subtraction: ",sub_result)
-# print("This is the sub.py file")
->>>>>>> ad21e82373138daf5ff5bb8ad920252403ed05d1
