61d3ea20580a01249a9ccb10c5e444646bc6fcb4
Rahul Kumar Ptel <patelrahulkumar002@gmail.com>
2025-03-28 14:16:33 +0530

    add file

---
+++ add.py
@@ -0,0 +1,9 @@
+class calculator:
+    def add_number(a,b):
+        return a+b
+    
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))   
+
+    add_result = add_number(num1,num2)
+    print("Subtraction: ",add_result)
